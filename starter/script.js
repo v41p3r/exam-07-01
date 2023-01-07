2e956976df6a03a68eaae774b01224f27ad17c76
Svetozar Stanev <svetozar.stanev@gmail.com>
2023-01-07 12:59:21 +0200

    starter

---
+++ starter/script.js
@@ -0,0 +1,6 @@
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+// this URL is for testing purposes to simulate failing request
+const failedRequest = "https://wronUrl.dog.ceo/api/breeds/image/random";
+// this URL is for testing purposes to simulate successful request, but falsy status
+const falsyResponseStatus = "https://dog.ceo/api/breeeeeds/image/random";
